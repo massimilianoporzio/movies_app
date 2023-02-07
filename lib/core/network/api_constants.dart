581536ea904770d629e6e0f8cdbb5c6db5c085c9
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-07 23:59:48 +0100

    gli endpoint hanno la apikey NON a compile time

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,10 +1,12 @@
+import '../envied/env.dart';
+
 class ApiConstants {
   static const baseUrl = 'https://api.themoviedb.org/3';
-  static const apiKey = "a88dcdfc2c28398374532e12def67676";
-  static const String nowPlayingMoviesPath =
+  static final apiKey = Env.tmdbApiKey;
+  static final String nowPlayingMoviesPath =
       "$baseUrl/movie/now_playing?api_key=$apiKey";
-  static const String popularMoviesPath =
+  static final String popularMoviesPath =
       "$baseUrl/movie/popular?api_key=$apiKey";
-  static const String topRatedMoviesPath =
+  static final String topRatedMoviesPath =
       "$baseUrl/movie/top_rated?api_key=$apiKey";
 }
